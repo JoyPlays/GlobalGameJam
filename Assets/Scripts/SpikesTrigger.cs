5bc51df19c66e2eab6b55335221a6f89e0c8ff2c
Flash <thebrainycraft@gmail.com>
2017-01-21 23:23:43 +0200

    New Terrain + MainGame scene

--- Assets/Scripts/SpikesTrigger.cs
+++ Assets/Scripts/SpikesTrigger.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class SpikesTrigger : MonoBehaviour {
 
-	private void OnTriggerEnter2D(Collider spike)
+	private void OnTriggerEnter(Collider spike)
 	{
-
+		if (spike.gameObject.CompareTag("Player"))
+		{
+			PlayerControler.Ded();
+			print("Collision");
+		}
 	}
 }
