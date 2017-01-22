718dbb59e12fe5d4fc4e42876d40ae7cbf88a582
raimonds <raimis0001@gmail.com>
2017-01-22 11:43:59 +0200

    Spikes with steam

---
+++ Assets/Scripts/Effects/ScaleEffect.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScaleEffect : MapActor
+{
+	[Range(0, 5)]
+	public float SteamTime;
+
+	public GameObject Colliders;
+	public Transform ScaleTransform;
+	// Use this for initialization
+	void Start()
+	{
+		key = "3";
+	}
+
+	protected override void DoAction()
+	{
+		StartCoroutine(Scale());
+	}
+
+	IEnumerator Scale()
+	{
+		float t = SteamTime;
+		while (t > 0)
+		{
+			float scale = Mathf.Lerp(0, 0.5f, t / SteamTime);
+
+			ScaleTransform.localScale = new Vector3(ScaleTransform.localScale.x, scale, ScaleTransform.localScale.z);
+
+			t -= Time.deltaTime;
+			yield return null;
+		}
+
+		Colliders.SetActive(false);
+		EndAnction();
+	}
+}
---
+++ Assets/Scripts/Effects/WaterFill.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterFill : MapActor
+{
+	public float topY;
+
+	[Range(0,3)]
+	public float FillTime = 2;
+	// Use this for initialization
+	void Start()
+	{
+		key = "2";
+	}
+	protected override void DoAction()
+	{
+		Debug.Log("Water fill action");
+		StartCoroutine(FillWatter());
+	}
+
+	IEnumerator FillWatter()
+	{
+		float t = FillTime;
+		float posY = transform.localPosition.y;
+		while (t > 0)
+		{
+			float y = Mathf.Lerp(posY, topY, 1 - t / FillTime);
+
+			transform.localPosition = new Vector3(transform.localPosition.x, y, transform.localPosition.z);
+
+			t -= Time.deltaTime;
+			yield return null;
+		}
+		EndAnction();
+		GetComponent<WaterIce>().enabled = true;
+		Destroy(this);
+	}
+}
