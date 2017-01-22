ae7cb3627f799ac37bb89bad9185eb70688f3f08
raimonds <raimis0001@gmail.com>
2017-01-22 10:14:11 +0200

    Watervave

---
+++ Assets/Scripts/Items/WatterWaves.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WatterWaves : MonoBehaviour
+{
+	[Range(-2, 2)]
+	public float speedX;
+	[Range(-2, 2)]
+	public float speedY;
+
+	[Range(0, 2)]
+	public float sine;
+	// Update is called once per frame
+	void Update()
+	{
+		float w = Mathf.Sin(Time.time * sine);
+		GetComponent<Renderer>().material.SetTextureOffset("_MainTex", new Vector2(w * speedX, Time.time * speedY));
+	}
+}
