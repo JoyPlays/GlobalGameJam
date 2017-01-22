f8add4a35e779a4a65197d5c077b97a4e96a1e12
raimonds <raimis0001@gmail.com>
2017-01-22 14:13:59 +0200

    boss

---
+++ Assets/Scripts/Effects/RockFall.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RockFall : MapActor
+{
+
+	public Transform Rocks;
+	public GameObject[] Pilars;
+
+	void Start()
+	{
+		key = "3";
+	}
+
+	protected override void DoAction()
+	{
+		StartCoroutine(BossFight());
+	}
+
+	IEnumerator BossFight()
+	{
+		yield return new WaitForSeconds(3);
+
+		foreach (GameObject obj in Pilars)
+		{
+			obj.gameObject.SetActive(false);
+		}
+		yield return new WaitForSeconds(0.2f);
+		foreach (Transform tr in Rocks)
+		{
+			Rigidbody rg = tr.gameObject.GetComponent<Rigidbody>();
+
+			rg.useGravity = true;
+			rg.isKinematic = false;
+
+			rg.AddForce(0, -9000, 0);
+			yield return null;
+		}
+
+		EndAnction();
+
+	}
+
+
+}
